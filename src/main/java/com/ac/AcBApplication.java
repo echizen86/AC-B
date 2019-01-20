a36051418e85740df1872114fc05022504d9b97b
echizen86 <jose860304@gmail.com>
2019-01-20 10:49:11 -0500

    Initial Commit

---
+++ src/main/java/com/ac/AcBApplication.java
@@ -0,0 +1,14 @@
+package com.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcBApplication.class, args);
+	}
+
+}
+
